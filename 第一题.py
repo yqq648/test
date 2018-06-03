5f787f14cde4e296b4166bdee1af66915421c979
yqq648 <648500318@qq.com>
2018-06-03 09:31:56 +0800

    这是第一次做，bigdata
    使用python做的第一个app，
    当前天气预报
    有一些小问题，希望看到的同学或者老师指出

---
+++ 第一题.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 15:05:39 2018
+
+@author: yq
+"""
+weeks = ['星期一',
+         '星期二',
+         '星期三',
+         '星期四',
+         '星期五',
+         '星期六',
+         '星期日']
+print(weeks[0])
+print(weeks[1])
+print(weeks[2])
+print(weeks[3])
+print(weeks[4])
+print('今天是：'+weeks[5])#注释，今天是星期六
+print(weeks[6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
